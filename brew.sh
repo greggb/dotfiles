e0a790cfbda7186764b599c8cd5484925f7a2bf2
Gregg B <514026+greggb@users.noreply.github.com>
2019-12-09 13:13:11 -0500

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,13 @@
+brew update
+brew upgrade
+BREW_PREFIX=$(brew --prefix)
+
+brew install bat
+brew install fd
+brew install fzf
+brew install git
+brew install htop
+brew install n
+brew install prettyping
+brew install the_silver_searcher
+brew install vim --with-override-system-vi
